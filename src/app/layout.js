529cb2cd4ece64348afadb949a1efa444ad0040e
Abhishek <Abhishekmishra@gmail.com>
2024-01-14 12:15:55 +0530

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import Footer from "@/components/footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next App",
-  description: "Next.js starter app",
+  title: "EpsilenPost",
+  description: "Next Js Project",
 };
 
 export default function RootLayout({ children }) {
